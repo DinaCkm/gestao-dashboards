b08cfe0a0d79910d75ae3c501a4b62945176ce6c
Financeiro <dina@ckmtalents.net>
2026-09-16 14:55:57 -0300

    feat(integracao): exportar dialogo de previa de email

--- client/src/features/programaIntegracao/components/index.ts
+++ client/src/features/programaIntegracao/components/index.ts
@@ -2,5 +2,5 @@ export { PainelSemana } from './PainelSemana';
 export { AgendaGeral } from './AgendaGeral';
 export { GerenciarPessoas } from './GerenciarPessoas';
 export { Indicadores } from './Indicadores';
+export { EmailPreviewDialog } from './EmailPreviewDialog';
 export { default as DetalheProcesso } from './DetalheProcesso';
-
