c04a7dca563c4cf55ebdfc0508ab7348c76a8d88
Financeiro <dina@ckmtalents.net>
2026-09-20 01:36:20 -0300

    Fix UI: habilita troca de tema na plataforma

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -263,7 +263,7 @@ function Router() {
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
+      <ThemeProvider defaultTheme="light" switchable>
         <RoleProvider>
           <TooltipProvider>
             <Toaster />
