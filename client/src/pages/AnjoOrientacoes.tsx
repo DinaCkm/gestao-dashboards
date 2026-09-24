85988e45e87f513059e2a54c220bf29759149dee
Financeiro <dina@ckmtalents.net>
2026-09-24 00:14:25 -0300

    Remove literal newline escape from Angel imports

--- client/src/pages/AnjoOrientacoes.tsx
+++ client/src/pages/AnjoOrientacoes.tsx
@@ -1,4 +1,5 @@
-import DashboardLayout from "@/components/DashboardLayout";\nimport AnjoRouteGuard from "@/features/programaIntegracao/components/AnjoRouteGuard";
+import DashboardLayout from "@/components/DashboardLayout";
+import AnjoRouteGuard from "@/features/programaIntegracao/components/AnjoRouteGuard";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Alert, AlertDescription } from "@/components/ui/alert";
